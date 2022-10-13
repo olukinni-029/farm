486efcc7fde4aa5b243f0f6b1d39dd8353b09821
olukinni_029 <olukinnif`150216029@gmail.com>
2022-10-13 11:16:11 +0100

    update fixed

--- services/passport.auth.js
+++ services/passport.auth.js
@@ -19,7 +19,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "https://farmpouch.herokuapp.com/auth/google/redirect",
-      // passReqToCallback: true
+      passReqToCallback: true
     },
     async (accessToken, refreshToken, profile, done) => {
       console.log(profile);
